cbcfa76199fe5cdb6e0f7fd2f9d2e725da9e8897
hsylla <hsylla@manh.com>
2022-11-26 13:05:06 -0500

    added auth screen

---
+++ lib/widgets/auth/auth_form.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+            child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Form(
+              child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(labelText: 'Email Address'),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Username'),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              ElevatedButton(onPressed: (() {}), child: Text('Login'))
+            ],
+          )),
+        )),
+      ),
+    );
+  }
+}
